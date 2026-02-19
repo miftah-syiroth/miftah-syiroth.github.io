c0221d57d288f250178c488cb617fcf1ada2f627
syiroth <irothsyiroth@gmail.com>
2026-02-19 10:13:23 +0700

    Update MCP configuration and add frontend rules; enhance welcome page layout with Bootstrap 5.3 styling

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -20,26 +20,22 @@ new #[Layout('layouts::guest')] class extends Component {
                 <p class="hero-description">
                     Universitas Harapan Bangsa, Purwokerto, Indonesia
                 </p>
-                <div class="mt-4 row g-4 justify-content-center">
-                    <div class="col-sm-6 col-md-6 col-12">
-                        <div class="card">
-                            <a href="" class="text-decoration-none">
-                                <div class="card-body">
-                                    <h5 class="card-title">English Language Education for Sustainable Futures:
-                                        Technology, Culture, and Global Competence</h5>
-                                </div>
-                            </a>
-                        </div>
+                <div class="row g-4 mt-2 justify-content-center conference-cards">
+                    <div class="col-12 col-md-6 col-lg-6 d-flex">
+                        <a href="{{ url('/english-education') }}" class="card text-decoration-none w-100 border border-2 border-primary shadow-sm h-100">
+                            <div class="card-body d-flex flex-column h-100">
+                                <h5 class="card-title text-primary">English Education</h5>
+                                <p class="card-text text-muted flex-grow-1">Technology, Culture, and Global Competence for Sustainable Futures</p>
+                            </div>
+                        </a>
                     </div>
-                    <div class="col-sm-6 col-md-6 col-12">
-                        <div class="card">
-                            <a href="" class="text-decoration-none">
-                                <div class="card-body">
-                                    <h5 class="card-title">Social Humanities: Economics, Management, and Law in Global
-                                        Contexts</h5>
-                                </div>
-                            </a>
-                        </div>
+                    <div class="col-12 col-md-6 col-lg-6 d-flex">
+                        <a href="{{ url('/social-humanities') }}" class="card text-decoration-none w-100 border border-2 border-success shadow-sm h-100">
+                            <div class="card-body d-flex flex-column h-100">
+                                <h5 class="card-title text-success">Social Humanities</h5>
+                                <p class="card-text text-muted flex-grow-1">Economics, Management, and Law in Global Contexts</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
